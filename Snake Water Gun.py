7c33deca570138fe1eeb7ee81674898f96a23d30
Muhammad Saad Nomani <107791651+saadnom@users.noreply.github.com>
2024-11-21 15:11:52 +0500

    Snake Water Gun.py

--- Snake Water Gun.py
+++ Snake Water Gun.py
@@ -1,8 +1,14 @@
 import random
 computer = random.choice([-1,0,1])
-youstr = input("Enter your choice: ")
 youDict = {'s':1,'g':0,'w':-1}
 reverseDict = {1:"Snake",0:'Gun',-1:'Water'}
+
+while True:
+    youstr = input("Enter your choice ('s','g','w'): ")
+    if youstr in youDict:
+        break
+    else:
+        print("Invalid")
 
 you = youDict[youstr]
 
@@ -12,7 +18,7 @@ if you == computer:
     print("It's draw")
 else:
     if you == -1 and computer == 1:
-        print("You Win!")
+        print("You Lost!")
     elif you == 1 and computer ==-1:
         print("You Win!")
     elif you == 0 and computer == 1:
